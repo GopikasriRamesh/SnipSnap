1f8819ce845ec53a000136d19d3b01cec4ccf6cb
GopikasriRamesh <srigopika270@gmail.com>
2025-07-27 17:35:45 +0530

    cors confiig

--- Backend/URLshortener/src/main/java/com/snipsnap/controller/URLController.java
+++ Backend/URLshortener/src/main/java/com/snipsnap/controller/URLController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/")
